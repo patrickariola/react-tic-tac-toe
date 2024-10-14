acc6b8cfe51c22319274ba9956ce9337e8d2cf30
patrickariola <patrickariola172@gmail.com>
2024-10-13 20:40:55 -1000

    Task 3
    End of Developer Tools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function Square({ value }){
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+function Square(){
+  const[value, setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
+  return (
+    <button
+    className="square"
+    onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+  
 }
 
 export default function Board() {
@@ -15,7 +31,7 @@ export default function Board() {
       <Square />
       <Square />
       </div>
-      <div className="board-row">
+    <div className="board-row">
       <Square />
       <Square />
       <Square />
